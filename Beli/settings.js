f4f3750fc60b361e908199a0ceb096f92ce218ad
Ahmadrijani <samsuljani174@gmail.com>
2026-01-10 19:28:56 +0700

    Create settings.js

---
+++ Beli/settings.js
@@ -0,0 +1,41 @@
+/* ================= KONFIGURASI UTAMA ================= */
+const appSettings = {
+  nomorWA: "6282351333047", // Format 628...
+  folderAsset: "asset/foto/", // Pastikan folder ini ada
+  maxDesign: 12, // Jumlah total gambar desain yang Anda miliki
+  
+  // Pesan default saat klik beli
+  pesanBeli: "Halo, saya mau pesan Mug Custom.",
+  
+  // Sosial Media (Akan muncul di Tab Kontak)
+  sosmed: [
+    {
+      nama: "Instagram Utama",
+      username: "@ahmadrijaniofficial",
+      url: "https://instagram.com/ahmadrijaniofficial",
+      icon: "fa-brands fa-instagram",
+      warna: "#E1306C"
+    },
+    {
+      nama: "Instagram Gelas",
+      username: "@mug_costum_11_tkj2",
+      url: "https://instagram.com/mug_costum_11_tkj2",
+      icon: "fa-brands fa-instagram",
+      warna: "#C13584"
+    },
+    {
+      nama: "YouTube",
+      username: "Ahmad Rijani Official",
+      url: "https://www.youtube.com/@ahmadrijaniofficial",
+      icon: "fa-brands fa-youtube",
+      warna: "#ff0000"
+    },
+    {
+      nama: "TikTok",
+      username: "@ahmadrijaniofficial",
+      url: "https://www.tiktok.com/@ahmadrijaniofficial",
+      icon: "fa-brands fa-tiktok",
+      warna: "#00f2ea"
+    }
+  ]
+};
