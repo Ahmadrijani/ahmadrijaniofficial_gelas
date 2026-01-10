b11db9d68854fa2e5cae5c5f33f6413ed5239af0
Ahmadrijani <samsuljani174@gmail.com>
2026-01-10 19:27:29 +0700

    Update and rename linkYT.json to link.js

---
+++ Beli/link.js
@@ -0,0 +1,41 @@
+// ===============================
+// DATA VIDEO YOUTUBE
+// ===============================
+
+const listVideo = [
+  {
+    judul: "Dokumentasi 1",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/CHnXdXhxOj8"
+  },
+  {
+    judul: "Dokumentasi 2",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/bd9NPOgudeI"
+  },
+  {
+    judul: "Dokumentasi 3",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/war8ZyYhveA"
+  },
+  {
+    judul: "Dokumentasi 4",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/XRVRdba7Q0o"
+  },
+  {
+    judul: "Dokumentasi 5",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/kIhLEATWjqk"
+  },
+  {
+    judul: "Dokumentasi 6",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/FAmVXdbeTWo"
+  },
+  {
+    judul: "Dokumentasi 7",
+    channel: "Ahmad Rijani",
+    url: "https://youtu.be/S9fFiWVK_no?si=o8HEUdOIIKgWIIC9"
+  }
+];
